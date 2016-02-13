c0234842e4b84287d746351c8556aa1abe61cd42
Salinder Sidhu <salinder.sid@gmail.com>
2016-02-13 06:01:46 -0500

    Added basic Bootstrap template for index page

---
+++ Game/js/index.js
@@ -0,0 +1,8 @@
+// Function that binds unobtrusive event handlers to HTML elements
+function initEventBindings() {
+    // Bind unobtrusive event handlers
+}
+
+
+// Bind unobtrusive event handlers to HTML pages as soon as the page loads
+window.onload = initEventBindings;
